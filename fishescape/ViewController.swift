050f9c85bf42c94946e03844ca240a81b3553545
新井山詠斗 <MAC-GIANTSPRIDE@araisaneito-no-macbook-air.local>
2016-05-18 20:31:44 +0900

    Initial Commit

---
+++ fishescape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fishescape
+//
+//  Created by 新井山詠斗 on 2016/05/18.
+//  Copyright © 2016年 新井山詠斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
