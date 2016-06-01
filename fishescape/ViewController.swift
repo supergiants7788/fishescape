a31cf5b09014d993ff53e42a62fad4d044e0a6fe
新井山詠斗 <supergiants7788@icloud.com>
2016-06-01 20:28:06 +0900

    6/1

--- fishescape/ViewController.swift
+++ fishescape/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var little: UIImageView!
+    let motionManager = CMMotionManager()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let width = self.view.frame.size.width
+        let height = self.view.frame.size.height
+        
+        motionManager.deviceMotionUpdateInterval = 1.0/60.0
+        motionManager.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler:
+            {
+                (deviceMotion:CMDeviceMotion?,error:NSError?) in
+                let fromX = self.little.center.x
+                let fromY = self.little.center.y
+                
+                if (fromX < 0){
+                    let xAngle = 180 * deviceMotion!.attitude.roll / M_PI
+                    let yAngle = 180 * deviceMotion!.attitude.pitch / M_PI
+                    let lenX = CGFloat(xAngle)
+                    if lenX > 0{
+                        let lenY = CGFloat(yAngle)
+                        var toX = fromX + lenX
+                        var toY = fromY + lenY
+                        self.little.center = CGPointMake(toX, toY)
+                    }
+                }else{
+                    let xAngle = 180 * deviceMotion!.attitude.roll / M_PI
+                    let yAngle = 180 * deviceMotion!.attitude.pitch / M_PI
+                    let lenX = CGFloat(xAngle)
+                    let lenY = CGFloat(yAngle)
+                    
+                    var toX = fromX + lenX
+                    var toY = fromY + lenY
+                    self.little.center = CGPointMake(toX, toY)
+                }
+            
+                if (fromX > width){
+                    let xAngle = 180 * deviceMotion!.attitude.roll / M_PI
+                    let yAngle = 180 * deviceMotion!.attitude.pitch / M_PI
+                    let lenX = CGFloat(xAngle)
+                    if lenX < 0{
+                        let lenY = CGFloat(yAngle)
+                        var toX = fromX + lenX
+                        var toY = fromY + lenY
+                        self.little.center = CGPointMake(toX, toY)
+                    }
+                }
+                
+            }
+        )
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
